c9179271d49a7d6e7aef7b4c8023bf16c638c4b1
Yale Yang <yalerkm25@gmail.com>
2023-02-09 17:55:55 -0800

    pass all tests for linkedList.js and tree.js

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -4,12 +4,74 @@ var LinkedList = function() {
   list.tail = null;
 
   list.addToTail = function(value) {
+    /*
+      create new node
+        what tail is referencing, earlier node reference new node
+        tail reference new node
+        set value at node at tail to value
+
+
+    */
+
+    var newNode = Node(value);
+
+    if (list.tail === null) {
+      list.head = newNode;
+      list.tail = newNode;
+
+      return newNode;
+    }
+
+    list.tail.next = newNode;
+    list.tail = newNode;
+    // list.tail.value = value; // not necessary
   };
 
+  // Node1(value,next=null)
+  // ^ tail,head (pointing to Node1)
+
+  //Node1(value,next=Node2)       -->         Node2(value,next=null)
+  // ^head                                      ^ tail (pointing to Node2)
+
+  // Node1(value,next=Node2) -->  Node2(value,next=Node3)    Node3(value,next=null).      newNode(value,next=null)
+  //                                                             ^ list.tail(pointing to Node3)
+
   list.removeHead = function() {
+    //move list.head one node down
+    var removedNode = list.head.value;
+    list.head = list.head.next;
+
+    return removedNode;
   };
 
   list.contains = function(target) {
+    /*
+      I value looking for
+      O boolean
+      C none
+      E no target being turned in.
+
+      iterate through linked list (reduce method) - doesn't work for linked lists
+        initial memo, false
+        for each node, does value === target?
+          true, return true
+          false, return memo or false
+
+      Plan B
+      iterate through linked list
+        if (val === target) {
+          return true
+        }
+      return false
+    */
+
+    for (var temp = list.head; temp !== null; temp = temp.next) {
+      if (temp.value === target) {
+        return true;
+      }
+    }
+
+    return false;
   };
 
   return list;
--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -3,17 +3,62 @@ var Tree = function(value) {
   newTree.value = value;
 
   // your code here
-  newTree.children = null;  // fix me
+  newTree.children = [];  // fix me
+  //all children?
 
+  _.extend(newTree, treeMethods);
   return newTree;
 };
 
 var treeMethods = {};
 
 treeMethods.addChild = function(value) {
+  /*
+  create a new Tree with a value of value
+  new Tree = newTree.childen[newTree.children.length]
+
+  */
+  var newChild = Tree(value);
+  this.children[this.children.length] = newChild;
+  return newChild;
 };
 
+// myTree.children[0]
+// typeof newTree.children MUST be an object or array
+// tree?
+
+// strategy 2:
+// create w wraparound function
+// call it on the root node
 treeMethods.contains = function(target) {
+  // initial strategy
+  // start at the top
+  // if newTree.children !== undefined
+    // iterate through it (recursively somehow)
+    // if newTree.value === target
+      // return true
+  // return false
+
+  // still works! function inside of contains:
+  // create recursionFunc
+    // first check if this.value === target
+      // return true
+    // if this.children
+      // iterate through children array
+        // call recursionFunc(this.children[i])
+    // return false;
+
+  // return recursionFunc(rootNode)
+
+  if (this.value === target) {
+    return true;
+  }
+  for (var i = 0; i < this.children.length; i++) {
+    if (this.children[i].contains(target)) {
+      return true;
+    }
+  }
+  return false;
 };
 
 
